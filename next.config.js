b4b5fc379ee4293d26105cd3a40a5c890e6ef8ea
Erik Portin <erik.portin@svt.se>
2018-12-10 21:19:29 +0100

    now 2

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const webpack = require('webpack');
-const withSass = require('@zeit/next-sass');
 
 const styleConfig = {
     cssModules: true,
@@ -10,18 +9,37 @@ const styleConfig = {
     }
 };
 
+const babelConf =  {webpack: config => {
+    config.plugins.push(
+        new webpack.EnvironmentPlugin({
+            GOOGLE_MAPS_API: '',
+            GOOGLE_STATIC_MAPS_API: '',
+        })
+    );
 
+    return config;
+}}
 
-module.exports = withSass({
-    webpack: config => {
-        config.plugins.push(
-            new webpack.EnvironmentPlugin({
-                GOOGLE_MAPS_API: '',
-                GOOGLE_STATIC_MAPS_API: '',
-            })
-        );
+const { PHASE_PRODUCTION_SERVER } =
+    process.env.NODE_ENV === 'development'
+        ? {} // We're never in "production server" phase when in development mode
+        : !process.env.NOW_REGION
+        ? require('next/constants') // Get values from `next` package when building locally
+        : require('next-server/constants'); // Get values from `next-server` package when building on now v2
 
-        return config;
-    },
-    ...styleConfig
-});
+module.exports = (phase, { defaultConfig }) => {
+    console.log('NODE_ENV', process.env.NODE_ENV)
+    console.log('NOW_REGION', process.env.NOW_REGION)
+    console.log('phase', phase)
+    if (phase === PHASE_PRODUCTION_SERVER) {
+        // Config used to run in production.
+        return babelConf;
+    }
+
+    const withSass = require('@zeit/next-sass');
+
+    return withSass({
+        babelConf,
+        ...styleConfig
+    })
+};
